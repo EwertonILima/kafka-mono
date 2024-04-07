5c94fc9c7b35bce4b9195a291f539b84f5407f0a
EwertonILima <ewertoninaciolima@gmail.com>
2024-04-07 19:45:58 -0300

    chore: :wrench: Implements Kafka producer config

---
+++ kafkaproducer/src/main/kotlin/com/ewertonilima/kafkaproducer/config/KafkaProducerConfig.kt
@@ -0,0 +1,39 @@
+package com.ewertonilima.kafkaproducer.config
+
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+@Configuration
+class KafkaProducerConfig(
+    private val kafkaProperties: KafkaProperties,
+    @Value("\${topics.payment.request.topic}")
+    private val paymentRequestTopic: String
+) {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, String> {
+        val properties = kafkaProperties.buildProducerProperties()
+        return DefaultKafkaProducerFactory(properties)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, String> {
+        return KafkaTemplate(producerFactory())
+    }
+
+    @Bean
+    fun paymentRequestTopicBuilder(): NewTopic {
+        return TopicBuilder
+            .name(paymentRequestTopic)
+            .partitions(1)
+            .replicas(1)
+            .build()
+    }
+}
