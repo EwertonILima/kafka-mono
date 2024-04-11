33ff7c93267bfee1e99aeb3fdc5caa030741dafd
ewertonilima <ewertoninaciolima@gmail.com>
2024-04-11 20:22:05 -0300

    chore: :wrench: Add dynamodb config
    ➕ Add spring aws dependencies
    ➕ Add spring aws dynamodb dependency

---
+++ kafkaconsumer/src/main/kotlin/com/ewertonilima/kafkaconsumer/config/DynamoDbConfig.kt
@@ -0,0 +1,19 @@
+package com.ewertonilima.kafkaconsumer.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import java.net.URI
+
+@Configuration
+class DynamoDbConfig {
+
+    @Bean
+    fun dynamoDbClient(): DynamoDbClient {
+        return DynamoDbClient.builder()
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .region(Region.SA_EAST_1)
+                .build()
+    }
+}
